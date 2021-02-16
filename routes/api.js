624541467d9ba4133a4f8ca3528a6c346cb2f2fb
Lisa <bardelli.lisa@gmail.com>
2021-02-16 15:19:20 +0000

    Database connected successfully

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const router = express.Router();
-const Todo = require('../models/todo');
+const Todo = require('../models/recipy');
 
 router.get('/todos', (req, res, next) => {
 
