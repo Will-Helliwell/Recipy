50d440f94d05deb3a554a123a06c4e07b553865a
AJ <aj.monty@gmail.com>
2021-02-17 14:46:26 +0000

    playerspec fail

--- spec/jasmine_examples/PlayerSpec.js
+++ spec/jasmine_examples/PlayerSpec.js
@@ -17,7 +17,7 @@ describe("Player", function() {
 
   it("should be able to play a Song", function() {
     player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
+    expect(player.currentlyPlayingSong).toEqual(blah);
 
     //demonstrates use of custom matcher
     expect(player).toBePlaying(song);
