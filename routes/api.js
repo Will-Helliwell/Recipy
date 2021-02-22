d29861c81278cc6cee2adc6f8e6ca76180e68bfb
Will-Helliwell <wj.helliwell@gmail.com>
2021-02-22 15:04:02 +0000

    filterButton now sending POST request to correct route

--- routes/api.js
+++ routes/api.js
@@ -13,20 +13,23 @@ Todo.find({}).skip((pageNumber-1)*paginate).limit(paginate)
 
 router.post('/todos', (req, res, next) => {
   if(req.body.action){
+    console.log("in add recipe route")
     Todo.create(req.body)
       .then(data => res.json(data))
       .catch(next)
   } else if (req.body.page) {
+    console.log("in next page route")
     var paginate = 2;
     const pageNumber = parseInt(req.body.page);
     Todo.find({}).skip((pageNumber-1)*paginate).limit(paginate)
     .then((data) => res.json(data))
   } else if (req.body.ingredient) {
-    // return "ingredient param recognised by post route"
+    console.log("in filter button route")
     var paginate = 1;
     Todo.find({}).skip((3)*paginate).limit(paginate)
     .then((data) => res.json(data))
   } else {
+    console.log("in error route")
     res.json({
       error: "The input field is empty"
     })
