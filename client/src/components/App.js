548f899b930ea692e4320f9f52de50a094f0f85c
AJ <aj.monty@gmail.com>
2021-02-22 16:04:33 +0000

    login and register working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "../App.css";
 import RecipeList from "./recipes/RecipeList";
 import IngredientList from "./ingredients/IngredientList";
@@ -7,6 +7,32 @@ import Navbar from "./layout/Navbar";
 import Landing from "./layout/Landing";
 import Register from "./auth/Register";
 import Login from "./auth/Login";
+import { Provider } from "react-redux";
+import store from "../store";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "../utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "../actions/authActions";
+
+import PrivateRoute from "./private-route/PrivateRoute";
+import Dashboard from "./dashboard/Dashboard";
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 function App() {
   const [ingredients, setIngredients] = useState({});
@@ -30,6 +56,7 @@ function App() {
   // Object.entries [['key', 'value'], ['key', 'value]]
   console.log("selectedIngredients", selectedIngredients);
   return (
+    <Provider store={store}>
     <Router>
     <div className="App">
   
@@ -37,6 +64,9 @@ function App() {
         <Route exact path="/" component={Landing} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
+        <Switch>
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        </Switch>
 
       <IngredientList
         ingredients={ingredients}
@@ -46,6 +76,7 @@ function App() {
  
     </div>
     </Router>
+    </Provider>
   );
 }
 
