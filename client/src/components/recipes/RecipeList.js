801f60bf569e65733ba8a7c2843bd0f0380777bf
ZeenLamDev <Zeen.Lam93@gmail.com>
2021-02-23 11:00:11 +0000

    makes post req for every checked ingredient and adds it to an array

--- client/src/components/recipes/RecipeList.js
+++ client/src/components/recipes/RecipeList.js
@@ -42,35 +42,48 @@ const RecipeList = ({ selectedIngredients }) => {
       });
   }, [pageNumber]);
 
-  // Good for performance as it only recalculates upon dependency changes
-  const filteredRecipes = useMemo(() => {
-    // Return the original list of recipes if no ingredients are selected
-    if (!selectedIngredients.length) return recipes;
-    // Loop through the recipes
-    const filteredRecipes = recipes.reduce((all, recipe) => {
-      // Because recipe.ingredients is an array we also have to loop through that
-      // in this case we're using find
-      const doesIngredientExist = recipe.ingredients.find((item) => {
-        // Loop through the checked ingredients and if ONE of the values of recipe.ingredients includes the selected ingredient key (e.g onion) then return true
-        const checkedIngredients = selectedIngredients.find((selectedItem) => {
-          return item.includes(selectedItem.toLowerCase());
-        });
-        return checkedIngredients;
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/todos`, {
+      method: "POST",
+      body: JSON.stringify({ ingredients: selectedIngredients }),
+    })
+      .then((data) => {
+        console.log("data", data);
+      })
+      .catch((err) => {
+        console.log("err", err);
       });
-      // If this is true then add it to the list
-      if (doesIngredientExist) {
-        return [...all, recipe];
-      }
-      // If the ingredient doesn't exist at all within recipe.ingredients then just return what we've got
-      return all;
-    }, []);
-    return filteredRecipes;
-  }, [selectedIngredients, recipes]);
+  }, [selectedIngredients]);
+
+  // Good for performance as it only recalculates upon dependency changes
+  // const filteredRecipes = useMemo(() => {
+  //   // Return the original list of recipes if no ingredients are selected
+  //   if (!selectedIngredients.length) return recipes;
+  //   // Loop through the recipes
+  //   const filteredRecipes = recipes.reduce((all, recipe) => {
+  //     // Because recipe.ingredients is an array we also have to loop through that
+  //     // in this case we're using find
+  //     const doesIngredientExist = recipe.ingredients.find((item) => {
+  //       // Loop through the checked ingredients and if ONE of the values of recipe.ingredients includes the selected ingredient key (e.g onion) then return true
+  //       const checkedIngredients = selectedIngredients.find((selectedItem) => {
+  //         return item.includes(selectedItem.toLowerCase());
+  //       });
+  //       return checkedIngredients;
+  //     });
+  //     // If this is true then add it to the list
+  //     if (doesIngredientExist) {
+  //       return [...all, recipe];
+  //     }
+  //     // If the ingredient doesn't exist at all within recipe.ingredients then just return what we've got
+  //     return all;
+  //   }, []);
+  //   return filteredRecipes;
+  // }, [selectedIngredients, recipes]);
 
   return (
     <>
       <>
-        {filteredRecipes.map((recipe, index) => {
+        {recipes.map((recipe, index) => {
           if (recipes.length === index + 1) {
             return (
               <div ref={lastRecipeElementRef}>
--- routes/api.js
+++ routes/api.js
@@ -16,43 +16,51 @@ router.get("/todos", async (req, res, next) => {
   });
 });
 
-router.get('/todos', (req, res, next) => {
-var paginate = 2;
-var pageNumber = 1;
-Todo.find({}).skip((pageNumber-1)*paginate).limit(paginate)
-          //this will return all the data, exposing only the id and action field to the client
-        .then((data) => res.json(data))
-        .catch(next);
+router.get("/todos", (req, res, next) => {
+  var paginate = 2;
+  var pageNumber = 1;
+  Todo.find({})
+    .skip((pageNumber - 1) * paginate)
+    .limit(paginate)
+    //this will return all the data, exposing only the id and action field to the client
+    .then((data) => res.json(data))
+    .catch(next);
 });
 
-router.post('/todos', (req, res, next) => {
-  if(req.body.action){
-    console.log("in add recipe route")
+router.post("/todos", (req, res, next) => {
+  if (req.body.action) {
+    console.log("in add recipe route");
     Todo.create(req.body)
       .then((data) => res.json(data))
       .catch(next);
   } else if (req.body.page) {
-    console.log("in next page route")
+    console.log("in next page route");
     var paginate = 2;
     const pageNumber = parseInt(req.body.page);
-    Todo.find({}).skip((pageNumber-1)*paginate).limit(paginate)
-    .then((data) => res.json(data))
+    Todo.find({})
+      .skip((pageNumber - 1) * paginate)
+      .limit(paginate)
+      .then((data) => res.json(data));
   } else if (req.body.ingredient) {
-    console.log("in filter button route")
-    Todo.find({ "$and": [ {ingredients: {$regex : ".*banana.*"}}, {ingredients: {$regex: ".*butter.*"}} ]})
-    .then((data) => res.json(data))
+    console.log("in filter button route");
+    Todo.find({
+      $and: [
+        { ingredients: { $regex: ".*banana.*" } },
+        { ingredients: { $regex: ".*butter.*" } },
+      ],
+    }).then((data) => res.json(data));
   } else {
-    console.log("in error route")
+    console.log("in error route");
     res.json({
       error: "The input field is empty",
     });
   }
 });
 
-router.delete('/todos/:id', (req, res, next) => {
-  Todo.findOneAndDelete({"_id": req.params.id})
-    .then(data => res.json(data))
-    .catch(next)
-})
+router.delete("/todos/:id", (req, res, next) => {
+  Todo.findOneAndDelete({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch(next);
+});
 
 module.exports = router;
