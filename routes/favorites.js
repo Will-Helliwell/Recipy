832abb60b29a84250003ffb1ce94b8b867a5d34c
AJ <aj.monty@gmail.com>
2021-02-24 12:36:26 +0000

    can now push via postman

--- routes/favorites.js
+++ routes/favorites.js
@@ -16,11 +16,14 @@ const User = require('../models/User')
 //   });
 
   router.post('/addfav', (req, res) => {
-    const user = User.findOne({ name: req.body.name} )
-    // user.schema.obj.favorites.push("hello")
-    // console.log(user.schema.obj)
-    // console.log(user.schema.obj.favorites)
+    // finds user
+    const user = User.findOne({ name: req.body.name } )
     return user
+    .then(result => {
+        // then adds recipe to user favorites
+        result.favorites.push(req.body.recipe)
+        console.log(result.favorites.length)
+    })
     //this will return all the data, exposing only the id and action field to the client
     .then((data) => res.json(data))
     .catch();
